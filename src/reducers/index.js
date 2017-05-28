8b2256c06efe74f1e1fabba321a4380ea1e7afd1
David Sellen <davidsellen@icloud.com>
2017-05-28 02:23:56 -0300

    creating / validating new item

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import PostsReducers from './reducer_posts';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducers,
+  form: formReducer
 });
 
 export default rootReducer;
